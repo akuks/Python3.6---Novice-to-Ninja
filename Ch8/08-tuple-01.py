a2c13ed335031e6a462b3e3fbd55e5b5e3082eca
akuks <kukreti.ashutosh@gmail.com>
2018-04-28 02:04:01 +0530

    Chapter-8 Tuples

---
+++ Ch8/08-tuple-01.py
@@ -0,0 +1,21 @@
+# Create Tuple
+
+tup = (1, 2, 3, "Hello", "World")
+
+print(tup)
+
+# Looping in Tuple
+
+for item in tup:
+    print (item)
+
+# in  operator
+
+b = 4 in tup
+print(b)
+
+# Not in Operator
+b = 4 not in tup
+print (b)
+
+

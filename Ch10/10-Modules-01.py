874bfaa6bd56a29be0aa4289aa69ab0b914c441c
akuks <kukreti.ashutosh@gmail.com>
2018-05-02 21:58:36 +0530

    Chapter-11 Add Exercises For Modules

---
+++ Ch10/10-Modules-01.py
@@ -0,0 +1,12 @@
+import calculator
+
+number1 = input("Enter the First Number: ")
+number2 = input("Enter the Second Number: ")
+
+print("Multiplication of two numbers is : ", calculator.multiply(number1, number2))
+
+print("Division of two numbers is : ", calculator.division(number1, number2))
+
+print("Addition of two numbers is :", calculator.addition(number1, number2))
+
+print("Subtraction of two numbers is :", calculator.subtraction(number1, number2))
---
+++ Ch10/calculator.py
@@ -0,0 +1,17 @@
+def multiply(number1, number2):
+    return number1 * number2
+
+
+def division(number1, number2):
+    return number1/number2
+
+
+def addition(number1, number2):
+    return number1 + number2
+
+
+def subtraction(number1, number2):
+    if number1 > number2:
+        return number1 - number2
+    else:
+        return number2 - number1
